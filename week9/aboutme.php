ca442ec6331f5fbee5c5f8c6c2986954e6155787
Jethro Rae Garcia <jbgarcia@student.apc.edu.ph>
2023-02-27 01:15:48 +0800

    week 9

---
+++ week9/aboutme.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> About Me </title>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<link rel="stylesheet" href='https://fonts.google.com/specimen/Poppins'>
+			<link rel="icon" type="image/x-icon" href="img/icon.png">
+			<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<ul>
+			<li><a href="references.html">References</a></li>
+			<li><a href="aboutme.html">About Me</a></li>
+			<li><a href="profilepage.html">Profile Page</a></li>
+		</ul>
+		<script>
+			function myFunction() {
+			alert('Hello');
+			}
+		</script>
+		<!--
+		<div class="references">
+			<h1 style="margin-left: auto; margin-right: auto"> TO BE FILLED IN </h1>
+		</div>
+		-->
+
+	<!-- Container for the image gallery -->
+	<div class="container">
+
+	<!-- Full-width images with number text -->
+	<div class="mySlides">
+	  <div class="numbertext">1 / 6</div>
+		<img src="img/photo1.jpg" style="width: 20%;">
+	</div>
+  
+	<div class="mySlides">
+	  <div class="numbertext">2 / 6</div>
+		<img src="img/photo2.jpg" style="width: 20%">
+	</div>
+  
+	<div class="mySlides">
+	  <div class="numbertext">3 / 6</div>
+		<img src="img/photo3.jpg" style="width: 20%">
+	</div>
+  
+	<div class="mySlides">
+	  <div class="numbertext">4 / 6</div>
+		<img src="img/photo4.jpg" style="width: 20%">
+	</div>
+  
+	<div class="mySlides">
+	  <div class="numbertext">5 / 6</div>
+		<img src="img/photo5.jpg" style="width: 20%">
+	</div>
+  
+	<div class="mySlides">
+	  <div class="numbertext">6 / 6</div>
+		<img src="img/photo6.jpg" style="width: 20%">
+	</div>
+  
+	<!-- Next and previous buttons -->
+	<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+	<a class="next" onclick="plusSlides(1)">&#10095;</a>
+  
+	<!-- Image text -->
+	<div class="caption-container">
+	  <p id="caption"></p>
+	</div>
+  </div> 
+		<button onclick="myFunction()">Try it</button>
+		<button onclick="myFunction()">Try it</button>
+		<p id="demo"></p>
+		<p>Click "Try it". Wait 3 seconds, and the page will alert "Hello".</p>
+		<button onclick="setTimeout(myFunction, 3000);">Try it</button>
+		<script>
+			let slideIndex = 1;
+				showSlides(slideIndex);
+
+			// Next/previous controls
+			function plusSlides(n) {
+				showSlides(slideIndex += n);
+				}
+
+				// Thumbnail image controls
+			function currentSlide(n) {
+				showSlides(slideIndex = n);
+				}
+
+			function showSlides(n) {
+				let i;
+				let slides = document.getElementsByClassName("mySlides");
+				let dots = document.getElementsByClassName("demo");
+				let captionText = document.getElementById("caption");
+					if (n > slides.length) {
+						slideIndex = 1
+					}
+					if (n < 1) {
+						slideIndex = slides.length
+					}
+					for (i = 0; i < slides.length; i++) {
+						slides[i].style.display = "none";
+					}
+					for (i = 0; i < dots.length; i++) {
+						dots[i].className = dots[i].className.replace(" active", "");
+					}
+				slides[slideIndex-1].style.display = "block";
+				dots[slideIndex-1].className += " active";
+				captionText.innerHTML = dots[slideIndex-1].alt;
+			} 
+
+			function myFunction() {
+					alert("This is the 'About Me' Section.");
+			}
+			function myFunction() {
+				let text;
+				let person = prompt("Please enter your name:", "Harry Potter");
+				if (person == null || person == "") {
+					text = "User cancelled the prompt.";
+				} else {
+					text = "Hello " + person + "! How are you today?";
+				}
+				document.getElementById("demo").innerHTML = text;
+			}
+			function myFunction() {
+				alert('Hello');
+			}
+		</script>
+	</body>
+</html>
---
+++ week9/profilepage.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+        <head>
+                <title>Profile Page</title>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <link rel="stylesheet" href='https://fonts.googleapis.com/css?family=Roboto'>
+                        <link rel="icon" type="image/x-icon" href="img/icon.png">
+                        <link rel="stylesheet" href="css/style.css">
+        </head>
+        <body>
+                <ul>
+                        <li><a href="references.html">References</a></li>
+                        <li><a href="aboutme.html">About Me</a></li>
+                        <li><a href="profilepage.html">Profile Page</a></li>
+                </ul>
+                <img class="img-pfp" src="img/pfp.jpg" alt="Profile Picture">
+                <div class="wrapper">
+                        <article class="about">
+                                <div class="demo">
+                                        <h1>I AM<br><br><span class="auto-type"></span></h1>
+                                </div>
+                                <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+                                <script> var typed = new Typed(".auto-type", {
+                                        strings: ["Jethro Rae<br><br>Garcia.", "a Singer.", "a Computer<br><br>Enthusiast.", "a Hobbyist.", "a RAM."],
+                                        typeSpeed: 65,
+                                        backSpeed: 75,
+                                        loop: true})
+                                </script>
+                        </article>
+                </div>
+        </body>
